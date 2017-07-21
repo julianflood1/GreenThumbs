920890f59713c17f4ef62837edb09ad84b0e795f
Julian Flood <Julian@Julians-MacBook-Pro.local>
2017-07-21 16:39:20 -0700

    delete spec files that were causing application to not build

--- src/app/age-sort.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AgeSortPipe } from './age-sort.pipe';
-
-describe('AgeSortPipe', () => {
-  it('create an instance', () => {
-    const pipe = new AgeSortPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
